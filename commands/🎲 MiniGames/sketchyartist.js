6ed859bfebec8b5cccd648ff79e84f2d5d29bbb4
cleivoid <90946440+cleivoid@users.noreply.github.com>
2022-02-04 19:33:44 +0700

    Update sketchyartist.js

--- commands/🎲 MiniGames/sketchyartist.js
+++ commands/🎲 MiniGames/sketchyartist.js
@@ -27,7 +27,7 @@ module.exports = {
             body: JSON.stringify({
                 max_age: 86400,
                 max_uses: 0,
-                target_application_id: "879864070101172255",
+                target_application_id: "879864104980979792",
                 target_type: 2,
                 temporary: false,
                 validate: null
